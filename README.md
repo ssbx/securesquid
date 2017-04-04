50fe11fcdf994b380c05d6213e27f622262856a3
Sébastien Serre <sserre.bx@gmail.com>
2017-04-04 13:36:06 +0200

    include squid

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Secure SQUID
 
 Ce dépot contient des utilitaires qui permettent de construire/activer squid avec toutes les fonctionnalités de sécurités disponnibles (actuellement, seulement ClamAv).
 
+## Create a local clamav build
+
+```sh
+$ ./generate.sh clamav_local
+$ ~/opt/clamav/bin/freshclam
+$ ~/opt/clamav/bin/clamscan -r ~
+```
+
+## Create a local squid build
+
+```sh
+$ ./generate.sh squid_local
+```
--- generate.sh
+++ generate.sh
@@ -1,56 +1,69 @@
 #!/bin/bash
 
 function clean_all {
-    rm -rf $HOME/clamav
+    rm -rf $HOME/opt/clamav
+    rm -rf $HOME/opt/squid
     cd sources/clamav
+    make clean
+    git clean -xdf
+    cd ../..
+    cd sources/squid
     make clean
     git clean -xdf
     cd ../..
 }
 
 function print_help {
-    echo "generate.sh <local|system|clean>"
+    echo "generate.sh <all|clamav|squid|clean>"
     echo " Options:"
-    echo "      local   build and test under local user"
-    echo "      system  build and deploy on system"
+    echo "      all     build squid and clamav"
+    echo "      squid   build squid"
+    echo "      clamav  build clamav"
     echo "      clean   clean all repos"
     echo ""
-    exit
 }
 
-function local_build {
+function squid_build {
+    clean_all
+    cd sources/squid
+    ./configure --prefix=$HOME/opt/squid 
+    make
+    make install
+}
+
+function clamav_build {
     clean_all
     cd sources/clamav
     ./configure \
-        --prefix=$HOME/clamav \
+        --prefix=$HOME/opt/clamav \
         --disable-clamav \
-        --with-systemdsystemunitdir=$HOME/clamav/systemd
+        --with-systemdsystemunitdir=$HOME/opt/clamav/systemd
     make
     make install
+    install -c -m 644 config/freshclam.conf config/clamd.conf $HOME/opt/clamav/etc
+    mkdir $HOME/opt/clamav/share/clamav
 }
 
-function system_build {
-    clean_all
-    cd sources/clamav
-    ./configure --prefix=/opt/clamav
-    make
-    make install
+function all {
+    clamav_build
+    squid_build
 }
-
 
 if  [ "$#" -ne 1 ]; then
     print_help
+    exit
 fi
-
 
 git submodule update --init
 
-if [ "$1" = "local" ]; then
-    local_build
-elif [ "$1" = "system" ]; then
-    system_build
+if [ "$1" = "all" ]; then
+    all
 elif [ "$1" = "clean" ]; then
     clean_all
+elif [ "$1" = "clamav" ]; then
+    clamav_build
+elif [ "$1" = "squid" ]; then
+    squid_build
 else
     print_help
 fi
